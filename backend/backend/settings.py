c8d207ab5ca042b50c9db2e40498c9dfc7219099
randomly <139393408+randomlyweb@users.noreply.github.com>
2024-08-31 21:03:30 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,12 +81,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         ),
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    ),
-    'DEFAULT_PARSER_CLASSES': (
-        'rest_framework.parsers.JSONParser',
-    )
 }
 
 CORS_ALLOWED_ORIGINS = [
